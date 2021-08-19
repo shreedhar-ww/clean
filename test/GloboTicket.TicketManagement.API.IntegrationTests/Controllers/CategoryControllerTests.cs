7cb5e776d7c6a2125d8a9f498cac77b02efb9520
acpkhan85 <acpkhan85@gmail.com>
2021-08-19 09:51:00 +0530

    Update CategoryControllerTests.cs

--- test/GloboTicket.TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
+++ test/GloboTicket.TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -19,21 +19,6 @@ namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers
             _factory = factory;
         }
 
-        [Fact]
-        public async Task ReturnsSuccessResult()
-        {
-            var client = _factory.GetAnonymousClient();
-
-            var response = await client.GetAsync("/api/V1/category/all");
-
-            response.EnsureSuccessStatusCode();
-
-            var responseString = await response.Content.ReadAsStringAsync();
-
-            var result = JsonConvert.DeserializeObject<Response<IEnumerable<CategoryListVm>>>(responseString);
-            
-            Assert.IsType<List<CategoryListVm>>(result.Data);
-            Assert.NotEmpty(result.Data);
-        }
+    
     }
 }
